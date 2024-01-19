13de8fc3c273fa5031be1bd7524e7ccc6616b3af
benamager <benaprivat@gmail.com>
2024-01-19 18:24:27 +0100

    New UI, now a actual gamepad

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
+const pollInterval = 25;
+
 var gamepad;
 var polling;
 
 window.addEventListener("gamepadconnected", function (e) {
-    console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.", e.gamepad.index, e.gamepad.id, e.gamepad.buttons.length, e.gamepad.axes.length);
     document.getElementById("status").textContent = "Connected";
     document.getElementById("id").textContent = e.gamepad.id;
+    document.querySelector(".statusIndicator").style.backgroundColor = "green";
+
     gamepad = e.gamepad;
 });
 
 window.addEventListener("gamepaddisconnected", function (e) {
-    console.log("Gamepad disconnected from index %d: %s", e.gamepad.index, e.gamepad.id);
     document.getElementById("status").textContent = "Disconnected";
     document.getElementById("id").textContent = "";
+    document.querySelector(".statusIndicator").style.backgroundColor = "red";
+
     gamepad = null;
 });
 
-/*  Mapping of buttons for PS5 controller, based on index
-    Dont know if they differ per controller as I only have a PS5 one for testing */
 const buttonMapping = {
     // Buttons
     3: "buttons_up",
@@ -36,23 +38,42 @@ const buttonMapping = {
     7: "rightTriggers_down",
 };
 
-// Reset the style of all buttons
+// Reset the gamepad input elements
 function resetButtons() {
-    Object.values(buttonMapping).forEach((buttonClass) => {
-        document.querySelector(`.${buttonClass}`).style.backgroundColor = "rgb(0, 0, 0, 0.1)";
+    // Buttons
+    Object.values(buttonMapping).forEach((buttonClass, index) => {
+        let element = document.querySelector(`.${buttonClass}`);
+        element.style.background = "rgb(0, 0, 0, 0.1)";
     });
+
+    // Stick positioning
     document.querySelector(".leftStick_dot").style.left = "50%";
     document.querySelector(".leftStick_dot").style.top = "50%";
     document.querySelector(".rightStick_dot").style.left = "50%";
     document.querySelector(".rightStick_dot").style.top = "50%";
+
+    // Stick opacity
+    document.querySelector(".leftStick_dot").style.background = "rgb(0, 0, 0, 0.1)";
+    document.querySelector(".rightStick_dot").style.background = "rgb(0, 0, 0, 0.1)";
 }
 
 function handleButtonPress(buttonIndex, value) {
     const action = buttonMapping[buttonIndex];
 
     if (action) {
-        console.log(`Button ${buttonIndex} pressed, corresponding to ${action}`);
-        document.querySelector(`.${action}`).style.backgroundColor = `rgb(0, 0, 0, ${value})`;
+        const element = document.querySelector(`.${action}`);
+
+        // Bottom left/right triggers which are pressure sensitive
+        if (buttonIndex === 6 || buttonIndex === 7) {
+            // Calculate gradient percentage
+            let percentage = value * 100;
+
+            // Apply a linear gradient from either left or right, and overlay it with black at 0.1 opacity
+            const to = buttonIndex === 6 ? "left" : "right";
+            element.style.background = `linear-gradient(to ${to}, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), linear-gradient(to ${to}, black ${percentage}%, transparent ${percentage}%)`;
+        } else {
+            element.style.backgroundColor = "rgba(0, 0, 0, 1)";
+        }
     }
 }
 
@@ -63,22 +84,24 @@ function handleAxisMove(axisIndex, value) {
     // Map the axis value from [-1, 1] to [0%, 100%]
     const position = ((value + 1) / 2) * 100;
 
-    // Left stick
-    // Horizontal movement
-    if (axisIndex === 0) {
-        leftStickDot.style.left = `${position}%`;
-    } else if (axisIndex === 1) {
-        // Vertical movement
-        leftStickDot.style.top = `${position}%`;
+    // Update position for left or right stick dot and calculate opacity
+    function handleStickDot(stick, axisIndex1, axisIndex2) {
+        stick.style.left = axisIndex === axisIndex1 ? `${position}%` : stick.style.left;
+        stick.style.top = axisIndex === axisIndex2 ? `${position}%` : stick.style.top;
+
+        const horizontalDistance = Math.abs((stick.style.left.replace("%", "") || 50) - 50);
+        const verticalDistance = Math.abs((stick.style.top.replace("%", "") || 50) - 50);
+        const maxDistance = Math.max(horizontalDistance, verticalDistance);
+        const opacity = Math.max(maxDistance / 50, 0.1);
+
+        stick.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
     }
 
-    // Right stick
-    // Horizontal movement
-    if (axisIndex === 2) {
-        rightStickDot.style.left = `${position}%`;
-    } else if (axisIndex === 3) {
-        // Vertical movement
-        rightStickDot.style.top = `${position}%`;
+    // Left or right stick
+    if (axisIndex === 0 || axisIndex === 1) {
+        handleStickDot(leftStickDot, 0, 1);
+    } else if (axisIndex === 2 || axisIndex === 3) {
+        handleStickDot(rightStickDot, 2, 3);
     }
 }
 
@@ -95,7 +118,7 @@ function startPolling() {
         if (gamepad) {
             // Button values
             gamepad.buttons.forEach((button, index) => {
-                if (button.pressed) {
+                if (button.value > 0) {
                     handleButtonPress(index, button.value);
                 }
             });
@@ -106,7 +129,13 @@ function startPolling() {
                 }
             });
         }
-    }, 20); // Poll every 100 milliseconds
+    }, pollInterval);
 }
-
 startPolling();
+
+// Firefox Gamepad API is lacking
+var isFirefox = typeof InstallTrigger !== "undefined";
+if (isFirefox) {
+    document.getElementById("error").textContent = "Firefox gamepad API is lacking, consider using any Chromium based browser instead.";
+    document.getElementById("error").style.display = "block";
+}
